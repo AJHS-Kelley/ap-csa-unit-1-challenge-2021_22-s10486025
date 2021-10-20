a0f5b2879ef7db3ff6ad70080bac64758694ea34
Ty W <71028905+s10486025@users.noreply.github.com>
2021-10-20 09:34:13 -0400

    v0.1

---
+++ DamageDealt.java
@@ -0,0 +1,14 @@
+// <Ty Whitecotten>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/21>, <9:33am>, v0.1
+
+public class DamageDealt { 
+
+    public static void main(String[] args) {
+        System.out.println("Welcome, Please name your weapon.");
+        
+
+
+    }
+
+    
+    
+}
